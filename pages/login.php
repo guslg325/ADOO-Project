c71002b11987eb853af251eb739740deb8b18f09
Villamil1 <71040931+Villamil1@users.noreply.github.com>
2021-11-21 18:21:27 -0600

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -5,20 +5,26 @@ if(!$conec)
 	die("Error en la base".mysqli_connect_error());
 }
 
-$nombre = $_POST['correo'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+$correo = $_POST['correo'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
 $pass = $_POST['contrasena'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
 
-$query = mysqli_query($conec,"SELECT * FROM login WHERE usuario = '".$nombre."' and password = '".$pass."'");//QUERY
-$vali = mysqli_num_rows($query);//CONTAMOS LAS COLUMNAS QUE ARROJA EL QUERY
-
-if($vali==1)//SI DETECTA UNA COLUMNA, ES QUE SI EXISTE EL USUARIO CON LA CONTRASENA
-{
-	header("Location: indexCliente.html");//MANDAMOS AL INDEX DEL CLIENTE
-	//echo "Bienvenido".$nombre;
-}
-else if ($vali == 0)//SI NO EXISTE COLUMNA, ES QUE NO EXISTE EL USUARIO EN LA BASE
-{
-	header("Location: login.html");//MANDAMOS A LA MISMA PAGINA DEL LOGIN
+if($correo=='' OR $pass==''){
+	header("Location: login.html");//MANDAMOS A LA MISMA PAGINA DE LOGIN
 	//echo "Error".$nombre;
+}
+else
+{
+	$query = mysqli_query($conec,"SELECT * FROM login WHERE correo = '".$correo."' and password = '".$pass."'");//QUERY
+	$vali = mysqli_num_rows($query);//CONTAMOS LAS COLUMNAS QUE ARROJA EL QUERY
+	if($vali==1)//SI DETECTA UNA COLUMNA, ES QUE SI EXISTE EL USUARIO CON LA CONTRASENA
+	{
+		header("Location: indexCliente.html");//MANDAMOS AL INDEX DEL CLIENTE
+		//echo "Bienvenido".$nombre;
+	}
+	else if ($vali == 0)//SI NO EXISTE COLUMNA, ES QUE NO EXISTE EL USUARIO EN LA BASE
+	{
+		header("Location: login.html");//MANDAMOS A LA MISMA PAGINA DEL LOGIN
+		//echo "Error".$nombre;
+	}
 }
 ?>
