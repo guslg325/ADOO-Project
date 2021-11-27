499e8c33074f5937dcb895560a56d6d7c6caa3f2
JavierSantiago95 <85073509+JavierSantiago95@users.noreply.github.com>
2021-11-27 17:22:30 -0600

    Added repeat mail verification

--- pages/registrar.php
+++ pages/registrar.php
@@ -1,6 +1,7 @@
 <?php
 $conec = mysqli_connect("localhost","root","","login");//("localhost","USUARIO","CONTRASENA","AQUI ES EL NOMBRE DE LA BASE")
 mysqli_set_charset($conec,"utf8");//Permite el uso de acentos en las vocales
+
 if(!$conec)
 {
 	die("Error en la base".mysqli_connect_error());
@@ -10,14 +11,25 @@ $correo = $_POST['correo'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
 $user = $_POST['usuario'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
 $pass = $_POST['contrasena'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
 
+
 if($correo=='' OR $user=='' OR $pass==''){
 	header("Location: registrar.html");//MANDAMOS A LA MISMA PAGINA DE REGISTRAR
 	//echo "Error".$nombre;
 }
 else
 {
-	$query = mysqli_query($conec,"INSERT INTO login VALUES('$correo','$user','$pass')");//QUERY
-	header("Location: login.html");
+	$verificar_correo = mysqli_query($conec,"SELECT * FROM login WHERE correo='$correo'");
+	if(mysqli_num_rows($verificar_correo) > 0){
+		echo '
+			<script>
+				alert("Ya existe una cuenta asociada a este correo");
+				window.location = "./registrar.html";
+			</script>
+		';
+	}
+	else{
+		$query = mysqli_query($conec,"INSERT INTO login VALUES('$correo','$user','$pass')");//QUERY
+	}
 }
 
 
@@ -29,4 +41,5 @@ else
 	header("Location: login.html");
 	//echo "Error".$nombre;
 }
+
 ?>
