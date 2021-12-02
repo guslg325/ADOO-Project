4897b4781e738614c0b756d6d970242843980860
JavierSantiago95 <85073509+JavierSantiago95@users.noreply.github.com>
2021-12-01 19:18:49 -0600

    Fixed listadoCentros

--- js/gestionarCentros.js
+++ js/gestionarCentros.js
@@ -27,7 +27,7 @@ msj content:
 */
 $(document).ready(function(){
 	$(".verCentro").click(function(){
-		let msj="<div class='row'><div class='col s12 m6 l4'><i class='fas fa-info'></i> ID de centro:</div><div class='col s12 m6 l8'>C0</div></div><div class='row'><div class='col s12 m6 l4'><i class='fas fa-map'></i> Ubicación:</div><div class='col s12 m6 l8'>Tlalpan, Ciudad de México</div></div><div class='row'><div class='col s12 m6 l4'><i class='fas fa-car'></i> Calle y número:</div><div class='col s12 m6 l8'>Calzada de Tlalpan 106</div></div><div class='row'><div class='col s12 m6 l4'><i class='fas fa-user'></i> Encargado asignado:</div><div class='col s12 m6 l8'>Enrique García López</div></div>";
+		let msj="<div class='row'><div class='col s12 m6 l4'><i class='fas fa-info'></i> ID de centro:</div><div class='col s12 m6 l8'>C0</div></div><div class='row'><div class='col s12 m6 l4'><i class='fas fa-map'></i> Ubicación:</div><div class='col s12 m6 l8'>Tlalpan, Ciudad de México</div></div><div class='row'><div class='col s12 m6 l4'><i class='fas fa-car'></i> Calle y número:</div><div class='col s12 m6 l8'>Calzada de Tlalpan #106</div></div><div class='row'><div class='col s12 m6 l4'><i class='fas fa-user'></i> Encargado asignado:</div><div class='col s12 m6 l8'>Enrique García López</div></div>";
 		$.dialog({
 			title:'<h5>Consultar centro de envío</h5>',
 			content:msj,
