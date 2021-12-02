92606732a1ef4e4951e7fbd6d1cfae0d4ff52ed3
Villamil1 <71040931+Villamil1@users.noreply.github.com>
2021-12-01 21:34:37 -0600

    Update registrar.js

--- js/registrar.js
+++ js/registrar.js
@@ -19,21 +19,26 @@ $(document).ready(function(){
                 data:$("form#formRegistro").serialize(),//Obtiene los datos del formulario y los serializa [name tag del elemento,valor]
                 cache:false,//Evita almacenar en cache (evita almacenar informacion basura)
                 success:function(respAX){//El argumento contendra la respuesta del php (un echo cuenta como respuesta)
-					//Si el php respondio entrara aqui
-                    let AX = JSON.parse(respAX);//Convierte la respuesta del php en JSON que se guarda en la variable AX
-                    $.alert({//Muestra un alert con los
-                        title:'<h5><i class="fas fa-info"></i> Aviso</h5>',
-                        content:AX.msj,//Mensaje personalizado dependiendo de la respuesta del php
-                        type:"orange",
-                        boxWidth: "50%",
-                        useBootstrap: false,
-                        onDestroy:function(){
-                        if(AX.codigo == 1)
-                            window.location.href = "./login.html";//Codigo de estado del php 1=exito, 0=error
-                        }
-                    });
-                }
-            });
-        }
-    });
+                    try {
+						//Si el php respondio entrara aqui
+                        let AX = JSON.parse(respAX);//Convierte la respuesta del php en JSON que se guarda en la variable AX
+                        $.alert({//Muestra un alert con los
+                            title:'<h5><i class="fas fa-info"></i> Aviso</h5>',
+                            content:AX.msj,//Mensaje personalizado dependiendo de la respuesta del php
+                            type:"orange",
+                            boxWidth: "50%",
+                            useBootstrap: false,
+                            onDestroy:function(){
+                                
+                            if(AX.codigo)
+                                window.location.href = "./login.html";//Codigo de estado del php 1=exito, 0=error
+							}
+						});
+					} catch (error) {
+						window.location.href = "./mailEnviado2.html";
+					}
+				}
+			});
+		}
+	});
 });
