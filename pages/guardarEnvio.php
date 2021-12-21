cf39346cd7020791185334fa78b37e6bc26aa8ec
Villamil1 <71040931+Villamil1@users.noreply.github.com>
2021-12-20 22:36:17 -0600

    Add files via upload

---
+++ pages/guardarEnvio.php
@@ -0,0 +1,60 @@
+<?php
+
+//Create an instance; passing `true` enables exceptions
+$conec = mysqli_connect("localhost","root","","squid");//("localhost","USUARIO","CONTRASENA","AQUI ES EL NOMBRE DE LA BASE")
+mysqli_set_charset($conec,"utf8");//Permite el uso de acentos en las vocales
+
+if(!$conec)
+{
+	die("Error en la base".mysqli_connect_error());
+}
+$datosOrigen=$_POST['selectTipoOrigen'];
+$datosDestino=$_POST['selectTipoDestino'];
+if($datosOrigen=='1'){
+    $nombreO = $_POST['nameOrigen'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $calleO = $_POST['calleOrigen'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $municipioO = $_POST['muniOrigen'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
+    $estadoO = $_POST['estadOrigen'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $cpO = $_POST['cpOrigenD'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $correoO = $_POST['correOrigen'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
+    $telefonoO = $_POST['telOrigen'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+
+}
+if($datosOrigen=='2'){
+    $centroO = $_POST['centroOrigenC'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $nombreO = $_POST['nameOrigenC'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $correoO = $_POST['correOrigenC'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
+    $telefonoO = $_POST['telOrigenC'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+}
+if($datosDestino=='1'){
+    $nombreD = $_POST['nameDestino'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $calleD = $_POST['calleDestino'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $municipioD = $_POST['muniDestino'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
+    $estadoD = $_POST['estadDestino'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $cpD = $_POST['cpDestinoD'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $correoD = $_POST['correDestino'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
+    $telefonoD = $_POST['telDestino'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+}
+if($datosDestino=='2'){
+    $centroD = $_POST['centroDestinoC'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $nombreD = $_POST['nameDestinoC'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    $correoD = $_POST['correDestinoC'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
+    $telefonoD = $_POST['telDestinoC'];//RECUPERAMOS LA VARABLE DEL FORMULARIO
+    //$query = mysqli_query($conec,"INSERT INTO envios VALUES(nombreO,calleO,municipioO,estadoO,cpO,correoO,telefonoO,nombreD,calleD,municipioD,estadoD,cpD,correoD,telefonoD,centroO,centroD,tipoPaquete)");//QUERY
+}
+$tipoPaquete = $_POST['tipoPaquete'];//RECUPERAMOS LA CONTRASENA DEL FORMULARIO
+if($datosOrigen=='1' && $datosDestino=='1'){
+    $query = mysqli_query($conec,"INSERT INTO envios VALUES('$nombreO','$calleO','$municipioO','$estadoO','$cpO','$correoO','$telefonoO','$nombreD','$calleD','$municipioD','$estadoD','$cpD','$correoD','$telefonoD',' ',' ','$tipoPaquete')");//QUERY
+}
+if($datosOrigen=='1' && $datosDestino=='2'){
+    $query = mysqli_query($conec,"INSERT INTO envios VALUES('$nombreO','$calleO','$municipioO','$estadoO','$cpO','$correoO','$telefonoO','$nombreD',' ',' ',' ',' ','$correoD','$telefonoD',' ','$centroD','$tipoPaquete')");//QUERY 
+}
+if($datosOrigen=='2' && $datosDestino=='1'){
+    $query = mysqli_query($conec,"INSERT INTO envios VALUES('$nombreO',' ',' ',' ',' ','$correoO','$telefonoO','$nombreD','$calleD','$municipioD','$estadoD','$cpD','$correoD','$telefonoD','$centroO',' ','$tipoPaquete')");//QUERY 
+}
+if($datosOrigen=='2' && $datosDestino=='2'){
+    $query = mysqli_query($conec,"INSERT INTO envios VALUES('$nombreO',' ',' ',' ',' ','$correoO','$telefonoO','$nombreD',' ',' ',' ',' ','$correoD','$telefonoD','$centroO','$centroD','$tipoPaquete')");//QUERY 
+}
+
+echo "GUARDADO CON EXITO";
+?>
